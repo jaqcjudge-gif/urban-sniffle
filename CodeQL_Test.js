7492b670e607e3a36de570b96e500404cea9708d
jaqcjudge-gif <jaq.c.judge@gmail.com>
2026-04-25 14:48:15 +0100

    Update CodeQL_Test.js

--- CodeQL_Test.js
+++ CodeQL_Test.js
@@ -1,3 +1,10 @@
 // Lets see if CodeQL can locate this bit of bad coding
-const userInput = "console.log('hello')";
-eval(userInput);
+const http = require('http');
+const url = require('url');
+
+http.createServer(function (req, res) {
+  const query = url.parse(req.url, true).query;
+  // Code injection vulnerability
+  eval(query.input);
+  res.end("done");
+}).listen(8080);
